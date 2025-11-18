7083b950207dd6900cea12efe6fc90c5da5d66c3
Adam-Grman <adamgrman32@gmail.com>
2025-11-18 22:04:22 +0100

    legacy offsets fixed and some new added

--- src/config.rs
+++ src/config.rs
@@ -30,12 +30,13 @@ pub const CONSTRAINTS: ik::Constraints = ik::Constraints {
     tibia_soffset: 35.0, // Offset to align tibia angle to straight down
 };
 
+//FIXME mirrored maybe reverse polarity?
 pub const SERVO_OFFSETS: ServoOffsets = ServoOffsets {
     left_front: (-2.5, -4.5, 0.0),
-    left_middle: (-5.0, -40.0, -1.5),
+    left_middle: (-5.0, -4.0, -1.5),
     left_back: (7.5, -10.0, -6.0),
     right_front: (2.5, 5.0, -3.0),
-    right_middle: (2.5, -40.0, 5.0),
+    right_middle: (2.5, -4.0, -5.0),
     right_back: (5.0, -1.5, -2.5),  // Fixed: was 5.0, should be -2.5 based on B1 data
 };
 
